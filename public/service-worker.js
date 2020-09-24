4af2adff464773cbebb226d22faacffe8aba202f
hila-abachi <igalk474@gmail.com>
2020-09-24 05:10:13 +0300

    pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,24 @@
-'use strict';
+const cacheName = 'stale-with-revalidate'
 
-// Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+// import workbox 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js')
+const { routing, strategies } = workbox
 
-// Add list of files to cache here.
-const FILES_TO_CACHE = [
-  '/offline.html',
-];
+// implements staleWhileRevalidate to all routes
+routing.registerRoute(
+  () => true,
+  new strategies.StaleWhileRevalidate({ cacheName }),
+)
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
 
-  evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
-  );
+// removes all caches not named <cacheName>
+const invalidateOldCache = async () => {
+  const keys = await caches.keys()
+  const isOldCache = (key) => key !== cacheName
+  const oldKeys = keys.filter(isOldCache)
 
-  self.skipWaiting();
-});
+  return Promise.all(oldKeys.map((key) => caches.delete(key)))
+}
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // Remove previous cached data from disk.
-  evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
-});
+// runs invalidateOldCache on activation
+self.addEventListener('activate', (e) => e.waitUntil(invalidateOldCache()))
